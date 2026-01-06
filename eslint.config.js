17b9fe9ff8a62a2126fdcb9539e0824383d8ad3b
Tom-Baj <bajouxtom@gmail.com>
2026-01-06 15:59:42 +0100

    Update package.json to set module type

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// ESLint 9.x flat config
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+];
+
+
